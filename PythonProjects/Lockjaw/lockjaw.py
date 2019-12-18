f6d64642a550d6493954385ad902333cb1f93015
Pwnbeard <59032613+Pwnbeard@users.noreply.github.com>
2019-12-18 13:25:14 -0800

    Delete lockjaw.py

--- PythonProjects/Lockjaw/lockjaw.py
+++
@@ -1,9 +0,0 @@
-import bluetooth
-
-nearby_devices = bluetooth.discover_devices(
-    duration=8, lookup_names=True, flush_cache=True, lookup_class=False
-    )
-print("Found {} devices.".format(len(nearby_devices)))
-
-for addr, name in nearby_devices:
-    print("  {} - {}".format(addr, name))
